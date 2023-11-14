e38a0d34072f71f599bc4c74fa5680f1ef7133ed
beyondlov1 <beyondlov1@hotmail.com>
2023-11-15 00:12:06 +0800

    add

--- background/background.js
+++ background/background.js
@@ -5,36 +5,54 @@
 */
 
 
-var currActiveUrl = "";
-var tabId2url = {}
+// var currActiveUrl = "";
+// var tabId2url = {}
 var windowActiveTabIdMap = {}
+
+function opennear(currActiveTabId, movingtab){
+  chrome.tabs.get(currActiveTabId, (activetab)=>{
+    if(!activetab) return;
+    if(activetab.groupId >= 0){
+      chrome.tabs.group({groupId:activetab.groupId, tabIds:[movingtab.id]}).then((groupId)=>{
+        chrome.tabs.move(movingtab.id, {index:activetab.index+1}).then(()=>{
+          chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            windowActiveTabIdMap[tabs[0].windowId] = tabs[0].id
+            console.log("after move"+windowActiveTabIdMap)
+          })        
+        })
+      })
+    }else{
+      chrome.tabs.move(movingtab.id, {index:activetab.index+1}).then(()=>{
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+          if(tabs.length > 0){
+            windowActiveTabIdMap[tabs[0].windowId] = tabs[0].id
+            console.log("after move"+windowActiveTabIdMap)
+          }
+        })
+      })
+    }
+  })
+}
 
 function handleCreated(tab) {
   console.log(tab.id);
   let currActiveTabId = windowActiveTabIdMap[tab.windowId] 
-  if (currActiveTabId && currActiveTabId == 0) return;
-  // tab.openerTabId
-  
-  chrome.tabs.get(currActiveTabId, (activetab)=>{
-    if(!activetab) return;
-    if(activetab.groupId >= 0){
-      chrome.tabs.group({groupId:activetab.groupId, tabIds:[tab.id]}).then((groupId)=>{
-        chrome.tabs.move(tab.id, {index:activetab.index+1})  
-      })
-    }else{
-      chrome.tabs.move(tab.id, {index:activetab.index+1})
-    }
-  })
-
+  if (!currActiveTabId || currActiveTabId == 0){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      opennear(tabs[0].id, tab)
+    })
+    return
+  }
+  opennear(currActiveTabId, tab)
 }
 chrome.tabs.onCreated.addListener(handleCreated);
 
 function handleUpdated(tabId, changeInfo, tab) {
   console.log(changeInfo.url);
-  if(changeInfo.url){
-    let oldurl = tabId2url[tabId]
-    tabId2url[tabId] = changeInfo.url;
-  }
+  // if(changeInfo.url){
+    // let oldurl = tabId2url[tabId]
+    // tabId2url[tabId] = changeInfo.url;
+  // }
 }
 chrome.tabs.onUpdated.addListener(handleUpdated);
 
@@ -43,10 +61,9 @@ function handleActivated(activeInfo) {
   try {
     if (activeInfo.tabId == 0) return;
     chrome.tabs.get(activeInfo.tabId, (tab)=>{
-      currActiveUrl = tab.url;
-      tabId2url[tab.id] = tab.url;
+      // currActiveUrl = tab.url;
+      // tabId2url[tab.id] = tab.url;
       windowActiveTabIdMap[tab.windowId] = tab.id
-      console.log("currActiveUrl"+currActiveUrl)
     })
   } catch (error) {
     console.error(error);
