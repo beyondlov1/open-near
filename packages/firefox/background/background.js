a79a028b2372b0ad6f59d8c2e6aac4775011ff65
beyondlov1 <beyondlov1@hotmail.com>
2024-01-14 00:35:08 +0800

    add

--- packages/firefox/background/background.js
+++ packages/firefox/background/background.js
@@ -5,22 +5,9 @@
 */
 
 
+// var currActiveUrl = "";
+// var tabId2url = {}
 var windowActiveTabIdMap = {}
-
-// chrome.storage.local.set({"windowActiveTabIdMap": {}}, function() {
-//   console.log('Data reset.');
-// });
-
-function save(windowId, tabId){
-  chrome.storage.local.get('windowActiveTabIdMap', function(result) {
-    let map = {}
-    if(result && Object.keys(result).length > 0 ) map = result["windowActiveTabIdMap"];
-    map[windowId] = tabId
-    chrome.storage.local.set({"windowActiveTabIdMap": map}, function() {
-      console.log('Data saved.');
-    });
-  });
-}
 
 function opennear(currActiveTabId, movingtab){
   if(currActiveTabId == movingtab.id) return;
@@ -32,9 +19,6 @@ function opennear(currActiveTabId, movingtab){
           chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
             windowActiveTabIdMap[tabs[0].windowId] = tabs[0].id
             console.log("after move"+windowActiveTabIdMap)
-
-            save(tabs[0].windowId, tabs[0].id)
-            
           })        
         })
       })
@@ -44,9 +28,6 @@ function opennear(currActiveTabId, movingtab){
           if(tabs.length > 0){
             windowActiveTabIdMap[tabs[0].windowId] = tabs[0].id
             console.log("after move"+windowActiveTabIdMap)
-
-            save(tabs[0].windowId, tabs[0].id)
-
           }
         })
       })
@@ -65,29 +46,14 @@ function handleCreated(tab) {
   }
   opennear(currActiveTabId, tab)
 }
-function handleCreated2(tab) {
-  chrome.storage.local.get('windowActiveTabIdMap', function(result) {
-    let windowActiveTabIdMap;
-    if(result){
-      windowActiveTabIdMap = result["windowActiveTabIdMap"]
-    }else{
-      windowActiveTabIdMap = {}
-    }
-    console.log(tab.id);
-    let currActiveTabId = windowActiveTabIdMap[tab.windowId] 
-    if (!currActiveTabId || currActiveTabId == 0){
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        opennear(tabs[0].id, tab)
-      })
-      return
-    }
-    opennear(currActiveTabId, tab)
-  })
-}
-chrome.tabs.onCreated.addListener(handleCreated2);
+chrome.tabs.onCreated.addListener(handleCreated);
 
 function handleUpdated(tabId, changeInfo, tab) {
   console.log(changeInfo.url);
+  // if(changeInfo.url){
+    // let oldurl = tabId2url[tabId]
+    // tabId2url[tabId] = changeInfo.url;
+  // }
 }
 chrome.tabs.onUpdated.addListener(handleUpdated);
 
@@ -96,8 +62,9 @@ function handleActivated(activeInfo) {
   try {
     if (activeInfo.tabId == 0) return;
     chrome.tabs.get(activeInfo.tabId, (tab)=>{
+      // currActiveUrl = tab.url;
+      // tabId2url[tab.id] = tab.url;
       windowActiveTabIdMap[tab.windowId] = tab.id
-      save(tab.windowId, tab.id)
     })
   } catch (error) {
     console.error(error);
